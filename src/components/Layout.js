74bfc4d1bd244f8721d63c60226a9fc82b14f9d4
mondirhallouli <halloulim@gmail.com>
2022-02-12 12:00:37 +0100

    moved the Cart styled components to the Layout file

--- src/components/Layout.js
+++ src/components/Layout.js
@@ -1,5 +1,7 @@
 import styled from "styled-components";
 import rem from "./Utils";
+import closeIcon from '../assets/close.svg';
+import deleteIcon from '../assets/delete-cart-item.svg';
 
 export const Container = styled.div`
   max-width: ${rem(940)};
@@ -13,6 +15,146 @@ export const HeaderCtaWrapper = styled.div`
   align-items: center;
   column-gap: ${rem(20)};
   row-gap: ${rem(16)};
+`;
+
+// SHOPPING CART COMPONENTS
+export const CartBox = styled.div`
+  width: 80vw;
+  height: 100vh;
+  padding: ${rem(32)};
+  background-color: white;
+  position: fixed;
+  top: 0;
+  left: ${(props) => props.cartOpen ? '0%' : '-100%'};
+  box-shadow: ${rem(10)} 0px ${rem(27)} -2px rgba(0,0,0,0.1);
+  z-index: 99;
+  transition: all 200ms ease-in-out;
+  overflow-y: auto;
+
+  @media (min-width: ${rem(376)}) {
+    width: 80vw;
+  }
+  
+  @media (min-width: ${rem(769)}) {
+    width: 70vw;
+  }
+`;
+
+export const CartTitle = styled.h2`
+  font-family: var(--heading-f);
+  font-size: ${rem(24)};
+  margin-bottom: ${rem(48)};
+  padding-bottom: ${rem(16)};
+  border-bottom: solid 1px #ddd;
+
+  @media (min-width: ${rem(769)}) {
+    font-size: ${rem(32)};
+  }
+`;
+
+export const ShoppingList = styled.div`
+  display: flex;
+  flex-direction: column;
+  align-items: center;
+  margin-bottom: ${rem(48)};
+  padding-bottom: ${rem(24)};
+  border-bottom: solid 1px #dddddd;
+`;
+
+export const CartItem = styled.div`
+  width: 100%;
+  display: grid;
+  grid-template-columns: auto 2fr auto;
+  gap: ${rem(16)};
+  margin-bottom: ${rem(28)};
+
+  @media (max-width: ${rem(768)}) {
+    grid-template-columns: 1fr;
+    gap: ${rem(13)};
+  }
+`;
+
+export const CartItemTitle = styled.h3`
+  font-size: ${rem(18)};
+  margin-bottom: ${rem(8)};
+`;
+
+export const CartCloseBtn = styled.button`
+  display: block;
+  width: ${rem(40)};
+  height: ${rem(40)};
+  background-image: url(${closeIcon});
+  background-repeat: no-repeat;
+  background-size: cover;
+  margin: 0 0 ${rem(30)} calc(100% - ${rem(32)});
+  position: relative;
+`;
+
+export const CartItemPrice = styled.h3`
+  font-size: ${rem(20)};
+  color: var(--wheat);
+`;
+
+export const CartItemInfo = styled.div`
+  justify-self: start;
+`;
+
+export const CartItemImg = styled.img`
+  display: block;
+  width: ${rem(60)};
+  height: auto;
+
+  @media (max-width: ${rem(768)}){
+    display: none;
+  }
+`;
+
+export const CartItemDelete = styled.button`
+  display: block;
+  width: ${rem(50)};
+  height: ${rem(50)};
+  padding: ${rem(12)} ${rem(24)};
+  background-image: url(${deleteIcon});
+  background-repeat: no-repeat;
+  background-size: 50%;
+  background-position: center;
+  background-color: var(--red);
+  border-radius: 50%;
+
+  &:hover {
+    opacity: 0.8;
+  }
+`;
+
+export const CartTotal = styled.div`
+  font-size: ${rem(32)};
+  font-weight: 700;
+  text-align: right;
+  margin: 0 0 ${rem(48)};
+  
+  span {
+    display: inline-block;
+    margin-left: ${rem(16)};
+    font-size: ${rem(34)};
+    color: var(--wheat);
+  }
+`;
+
+export const CartCheckout = styled.button`
+  display: block;
+  width: 50%;
+  padding: ${rem(16)};
+  margin: auto;
+  background-color: var(--purple);
+  color: var(--white);
+
+  &:hover {
+    opacity: 0.9;
+  }
+
+  @media (max-width: ${rem(768)}) {
+    width: 80%;
+  }
 `;
 
 // SHOP SECTION COMPONENTS
--- src/components/header/Cart.js
+++ src/components/header/Cart.js
@@ -1,148 +1,20 @@
 import { useDispatch, useSelector } from 'react-redux';
-import styled from 'styled-components';
 import { removeFromCart } from '../../state/cart/shopSlice';
-import rem from '../Utils';
-import closeIcon from '../../assets/close.svg';
-import deleteIcon from '../../assets/delete-cart-item.svg';
+import {
+  CartBox,
+  CartCloseBtn,
+  CartTitle,
+  ShoppingList,
+  CartItem,
+  CartItemImg,
+  CartItemInfo,
+  CartItemTitle,
+  CartItemPrice,
+  CartItemDelete,
+  CartTotal,
+  CartCheckout
+} from '../Layout';
 
-const CartBox = styled.div`
-  width: 80vw;
-  height: 100vh;
-  padding: ${rem(32)};
-  background-color: white;
-  position: fixed;
-  top: 0;
-  left: ${(props) => props.cartOpen ? '0%' : '-100%'};
-  box-shadow: ${rem(10)} 0px ${rem(27)} -2px rgba(0,0,0,0.1);
-  z-index: 99;
-  transition: all 200ms ease-in-out;
-  overflow-y: auto;
-
-  @media (min-width: ${rem(376)}) {
-    width: 80vw;
-  }
-  
-  @media (min-width: ${rem(769)}) {
-    width: 70vw;
-  }
-`;
-
-const CartTitle = styled.h2`
-  font-family: var(--heading-f);
-  font-size: ${rem(24)};
-  margin-bottom: ${rem(48)};
-  padding-bottom: ${rem(16)};
-  border-bottom: solid 1px #ddd;
-
-  @media (min-width: ${rem(769)}) {
-    font-size: ${rem(32)};
-  }
-`;
-
-const ShoppingList = styled.div`
-  display: flex;
-  flex-direction: column;
-  align-items: center;
-  margin-bottom: ${rem(48)};
-  padding-bottom: ${rem(24)};
-  border-bottom: solid 1px #dddddd;
-`;
-
-const CartItem = styled.div`
-  width: 100%;
-  display: grid;
-  grid-template-columns: auto 2fr auto;
-  gap: ${rem(16)};
-  margin-bottom: ${rem(28)};
-
-  @media (max-width: ${rem(768)}) {
-    grid-template-columns: 1fr;
-    gap: ${rem(13)};
-  }
-`;
-
-const CartItemTitle = styled.h3`
-  font-size: ${rem(18)};
-  margin-bottom: ${rem(8)};
-`;
-
-const CartCloseBtn = styled.button`
-  display: block;
-  width: ${rem(40)};
-  height: ${rem(40)};
-  background-image: url(${closeIcon});
-  background-repeat: no-repeat;
-  background-size: cover;
-  margin: 0 0 ${rem(30)} calc(100% - ${rem(32)});
-  position: relative;
-`;
-
-const CartItemPrice = styled.h3`
-  font-size: ${rem(20)};
-  color: var(--wheat);
-`;
-
-const CartItemInfo = styled.div`
-  justify-self: start;
-`;
-
-const CartItemImg = styled.img`
-  display: block;
-  width: ${rem(60)};
-  height: auto;
-
-  @media (max-width: ${rem(768)}){
-    display: none;
-  }
-`;
-
-const CartItemDelete = styled.button`
-  display: block;
-  width: ${rem(50)};
-  height: ${rem(50)};
-  padding: ${rem(12)} ${rem(24)};
-  background-image: url(${deleteIcon});
-  background-repeat: no-repeat;
-  background-size: 50%;
-  background-position: center;
-  background-color: var(--red);
-  border-radius: 50%;
-
-  &:hover {
-    opacity: 0.8;
-  }
-`;
-
-const CartTotal = styled.div`
-  font-size: ${rem(32)};
-  font-weight: 700;
-  text-align: right;
-  margin: 0 0 ${rem(48)};
-  
-  span {
-    display: inline-block;
-    margin-left: ${rem(16)};
-    font-size: ${rem(34)};
-    color: var(--wheat);
-  }
-`;
-
-const CartCheckout = styled.button`
-  display: block;
-  width: 50%;
-  padding: ${rem(16)};
-  margin: auto;
-  background-color: var(--purple);
-  color: var(--white);
-
-  &:hover {
-    opacity: 0.9;
-  }
-
-  @media (max-width: ${rem(768)}) {
-    width: 80%;
-  }
-`;
 
 const Cart = ({ cartOpen, toggleCart }) => {
 
@@ -182,4 +54,4 @@ const Cart = ({ cartOpen, toggleCart }) => {
   )
 }
 
-export default Cart
+export default Cart;
